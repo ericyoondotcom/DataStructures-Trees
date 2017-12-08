b7df310026453839b1893339775ec6a3c40d7fa2
Eric <emjyoon@gmail.com>
2017-12-07 17:12:26 -0800

    Finished, but getting a weird error

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -13,42 +13,16 @@ namespace Trees
 {
     class MainClass
     {
-        
-		public static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
 
             Console.WriteLine("Hello World!");
+            RBTree<int> tree = new RBTree<int>();
 
-            var tree = new AVLTree<int>();
-            /* tree.Add(10);
-             tree.Add(8);
-             tree.Add(15);
-             tree.Add(16);
-             tree.Add(13);
-             tree.Add(14);
-             tree.Add(12);
-             *//*
             tree.Add(10);
-            tree.Add(7);
-            tree.Add(8);
-            tree.Add(6);
-            tree.Add(5);
-            tree.Add(4);*/
-            /*tree.Add(10);
-            tree.Add(1);
-            tree.Add(13);*/
-            /*tree.Add(14);
-            tree.Add(16);
-            tree.Add(15);*/
-        
-  
-            Random randy = new Random();
-            Console.WriteLine("Running the AlexTester machine!");
-            for (int i = 0; i < 15; i++){
-                var rando = randy.Next(0, 20);
-                Console.WriteLine(rando);
-                tree.Add(rando);
-            }
+            tree.Add(11);
+            tree.Add(12);
         }
     }
 }
--- Trees/RBTree.cs
+++ Trees/RBTree.cs
@@ -78,20 +78,168 @@ namespace Trees
         
         void InsertCheck(RBNode<T> me)
         {
-            if(me != topNode)
+            if(me != topNode) //If x is root, change color to BLACK
             {
+                //Do the following if x's parent is not Black or x is not root
                 RBNode<T> uncle = (me.parent.parent.left == me.parent) ? me.parent.parent.right : me.parent.parent.left;
 
-                if(uncle.color == Enums.Colors.red){
-                    me.parent.color = Enums.Colors.black;
-                    uncle.color = Enums.Colors.black;
-                    me.parent.parent.color = Enums.Colors.red;
-                    InsertCheck(me.parent.parent);
-                }else{
-                    
+                if(uncle.color == Enums.Colors.red){ //if x's uncle is red
+                    me.parent.color = Enums.Colors.black; //change color of parent to black
+                    uncle.color = Enums.Colors.black; //change color of uncle to black
+                    me.parent.parent.color = Enums.Colors.red; //make grandparent red
+                    InsertCheck(me.parent.parent); //rule check my grandparent
+                }else{ //if x's uncle is black
+                    if (me.parent.right == me){ //if i'm a right child
+						if (me.parent.parent.right == me.parent)
+						{
+							//right-right case
+							RotateNodeLeft(me.parent.parent); //rotate grandparent left
+                            //and swap grandparent and parent colors
+							var swapColor = me.parent.color;
+							me.parent.color = me.parent.parent.color;
+							me.parent.parent.color = swapColor;
+
+						}
+						else if (me.parent.parent.left == me.parent)
+						{
+                            //left-right case
+                            RotateNodeLeft(me.parent); //rotate parent left
+							if (me.parent.parent.left == me.parent) //and check left-left case
+							{
+								//left-left case
+								RotateNodeRight(me.parent.parent); //rotate grandparent right
+								//and swap grandparent and parent colors
+								var swapColor = me.parent.color;
+								me.parent.color = me.parent.parent.color;
+								me.parent.parent.color = swapColor;
+							}
+
+						}
+						else
+						{
+							throw new Exception("AAH! My grandparent disowned my parent!");
+						}
+                    }else if (me.parent.left == me){ //if i'm a left child
+						if (me.parent.parent.right == me.parent)
+						{
+                            //right-left case
+                            RotateNodeRight(me.parent);
+							if (me.parent.parent.right == me.parent)
+							{
+								//right-right case
+								RotateNodeLeft(me.parent.parent); //rotate grandparent left
+								//and swap grandparent and parent colors
+								var swapColor = me.parent.color;
+								me.parent.color = me.parent.parent.color;
+								me.parent.parent.color = swapColor;
+
+							}
+
+						}
+						else if (me.parent.parent.left == me.parent)
+						{
+                            //left-left case
+                            RotateNodeRight(me.parent.parent); //rotate grandparent right
+                            //and swap grandparent and parent colors
+                            var swapColor = me.parent.color;
+                            me.parent.color = me.parent.parent.color;
+                            me.parent.parent.color = swapColor;
+						}
+						else
+						{
+							throw new Exception("AAH! My grandparent disowned my parent!");
+						}
+                    }else{
+                        throw new Exception("AAH! The parents disowned the added node!");
+                    }
                 }
             }
             topNode.color = Enums.Colors.black;
         }
+
+
+
+        void RotateNodeLeft(RBNode<T> node){
+			Console.WriteLine("Doing a left rotation!");
+			node.right.right = node.right?.right;
+
+			if (node.right.right != null)
+			{
+				node.right.right.parent = node.right;
+			}
+
+			node.right = node.right.left;
+			if (node.left != null)
+			{
+				node.left.parent = node;
+			}
+
+			if (node.parent == null)
+			{
+				topNode = node.right;
+
+			}
+			else
+			{
+
+				if (node.parent.left == node)
+				{
+					node.parent.left = node.right;
+				}
+				else if (node.parent.right == node)
+				{
+					node.parent.right = node.right;
+				}
+				else
+				{
+					throw new Exception("Luke... I'm... Not... Your... Father!");
+				}
+			}
+			node.right.parent = node.parent;
+
+			node.right.left = node;
+			node.parent = node.right;
+        }
+
+        void RotateNodeRight(RBNode<T> node){
+			Console.WriteLine("Doing a right rotation!");
+
+			node.left.left = node.left?.left;
+			if (node.left.left != null)
+			{
+				node.left.left.parent = node.left;
+			}
+			node.left = node.left.right;
+			if (node.left.right != null)
+				node.left.right.parent = node;
+
+
+			if (node.parent == null)
+			{
+				topNode = node.left;
+			}
+			else
+			{
+
+				if (node.parent.left == node)
+				{
+					node.parent.left = node.left;
+				}
+				else if (node.parent.right == node)
+				{
+					node.parent.right = node.left;
+				}
+				else
+				{
+					throw new Exception("Luke... I'm... Not... Your... Father!");
+				}
+			}
+			node.left.parent = node.parent;
+			node.left.right = node;
+            node.parent = node.left;
+        }
+
+
+
     }
 }
